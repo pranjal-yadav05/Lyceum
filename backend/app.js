c5640ffc789a89326a81f12630aed87818fec862
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2024-12-22 18:42:27 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -92,6 +92,6 @@ io.on('connection', (socket) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
