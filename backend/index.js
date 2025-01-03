c80abd0dd451dca133b2e023f98b4cd0d05215fc
pranjal-yadav05 <pranjalyadavpy212005@gmail.com>
2025-01-03 16:21:00 +0530

    Revert "Update index.js"
    This reverts commit 88cfedbf70e6e6044e915b5a383b6b727700d7d3.

--- backend/index.js
+++ backend/index.js
@@ -67,7 +67,7 @@ app.get('/', (req, res) => res.send("Welcome to the server..."));
 // Setup Socket.IO
 const io = new Server(httpServer, {
   cors: {
-    origin: '*',
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
